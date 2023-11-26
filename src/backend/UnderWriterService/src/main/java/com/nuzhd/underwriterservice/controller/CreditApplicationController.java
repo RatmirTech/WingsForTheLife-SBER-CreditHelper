d06cf2130633df27712e6e638931b9100386ed80
yeeeip <dmit.nujdin2014@yandex.ru>
2023-11-26 11:03:53 +0300

    api

--- src/backend/UnderWriterService/src/main/java/com/nuzhd/underwriterservice/controller/CreditApplicationController.java
+++ src/backend/UnderWriterService/src/main/java/com/nuzhd/underwriterservice/controller/CreditApplicationController.java
@@ -5,11 +5,13 @@ import com.nuzhd.underwriterservice.service.CreditApplicationService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1/applications")
 public class CreditApplicationController {
 
     private final CreditApplicationService creditApplicationService;
